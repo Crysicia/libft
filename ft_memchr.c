84eeb06f96f477a09f024aa99661f07cf8e86390
Crysicia <7459685+Crysicia@users.noreply.github.com>
2020-08-04 23:18:06 +0200

    Add ft_memchr.c and fix ft_memmove.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,33 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpassera <lpassera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/04 17:55:12 by lpassera          #+#    #+#             */
-/*   Updated: 2020/08/04 17:55:13 by lpassera         ###   ########.fr       */
+/*   Created: 2020/08/04 23:08:39 by lpassera          #+#    #+#             */
+/*   Updated: 2020/08/04 23:08:41 by lpassera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			index;
-	unsigned char	*ptr_src;
-	unsigned char	*ptr_dst;
+	size_t index;
 
-	if (src == NULL && dst == NULL)
-		return (NULL);
 	index = -1;
-	ptr_src = (unsigned char *)src;
-	ptr_dst = (unsigned char *)dst;
-	if (src > dst)
-		while (++index < len)
-			ptr_dst[index] = ptr_src[index];
-	else
-		while (++index < len)
-			ptr_dst[len - index - 1] = ptr_src[len - index - 1];
-	return (dst);
+	while (++index < n)
+		if (*(unsigned char *)(s + index) == (unsigned char)c)
+			return ((void *)(s + index));
+	return (NULL);
 }
