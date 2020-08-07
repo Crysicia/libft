002a1ddb99adefb2e847004a5cf001bdd8cd8840
Crysicia <7459685+Crysicia@users.noreply.github.com>
2020-08-07 12:21:56 +0200

    Add strlcat and fix memcmp & strncmp

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,29 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpassera <lpassera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/04 23:38:10 by lpassera          #+#    #+#             */
-/*   Updated: 2020/08/04 23:38:14 by lpassera         ###   ########.fr       */
+/*   Created: 2020/08/07 12:20:27 by lpassera          #+#    #+#             */
+/*   Updated: 2020/08/07 12:20:28 by lpassera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
 {
-	size_t			index;
-	unsigned char	*ptr_s1;
-	unsigned char	*ptr_s2;
+	size_t dstlen;
+	size_t srclen;
+	size_t index;
 
+	dstlen = 0;
+	srclen = 0;
 	index = 0;
-	ptr_s1 = (unsigned char *)s1;
-	ptr_s2 = (unsigned char *)s2;
-	if (n == 0)
-		return (0);
-	while (index < n - 1 && ptr_s1[index] == ptr_s2[index])
-		index++;
-	return (ptr_s1[index] - ptr_s2[index]);
+	while (dst[dstlen])
+		dstlen++;
+	while (src[srclen])
+		srclen++;
+	if (dstsize > 0)
+	{
+		while (index + dstlen < dstsize - 1 && src[index])
+		{
+			dst[dstlen + index] = src[index];
+			index++;
+		}
+		dst[dstlen + index] = '\0';
+	}
+	if (dstsize < dstlen)
+		return (dstsize + srclen);
+	return (srclen + dstlen);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,8 +16,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t index;
 
+	if (n == 0)
+		return (0);
 	index = 0;
-	while (s1[index] && s1[index] == s2[index] && index < n)
+	while (s1[index] && s1[index] == s2[index] && index < n - 1)
 		index++;
 	return ((unsigned char)s1[index] - (unsigned char)s2[index]);
 }
