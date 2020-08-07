e427dff216f75d2fd5728bf70681f18e8ff8ae56
Crysicia <7459685+Crysicia@users.noreply.github.com>
2020-08-07 14:36:45 +0200

    Add ft_strdup.c

--- libft.h
+++ libft.h
@@ -27,6 +27,7 @@ size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 /* Custom functions */
 int		ft_isspace(int c);
